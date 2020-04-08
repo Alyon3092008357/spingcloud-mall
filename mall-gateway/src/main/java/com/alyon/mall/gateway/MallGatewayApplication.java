6abf8bc38669616bc6c102aaee7e4185e1edea23
alyon <1247956257@qq.com>
2020-04-09 06:06:45 +0800

    动态路由配置

---
+++ mall-gateway/src/main/java/com/alyon/mall/gateway/MallGatewayApplication.java
@@ -0,0 +1,11 @@
+package com.alyon.mall.gateway;
+
+/**
+ * @ClassName MallGatewayApplication
+ * @Description: TODO
+ * @Author liang
+ * @Date 2020/4/9
+ * @Version V1.0
+ **/
+public class MallGatewayApplication {
+}
---
+++ mall-gateway/src/main/java/com/alyon/mall/gateway/config/DynamicRouteConfig.java
@@ -0,0 +1,11 @@
+package com.alyon.mall.gateway.config;
+
+/**
+ * @ClassName DynamicRouteConfig
+ * @Description: TODO
+ * @Author liang
+ * @Date 2020/4/9
+ * @Version V1.0
+ **/
+public class DynamicRouteConfig {
+}
