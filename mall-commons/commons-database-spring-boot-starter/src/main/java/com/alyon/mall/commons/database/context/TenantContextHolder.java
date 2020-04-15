5d4360d80326ca493896f2a0a0e187b134a919ae
alyon <1247956257@qq.com>
2020-04-16 02:42:35 +0800

    数据库访问启动插件

---
+++ mall-commons/commons-database-spring-boot-starter/src/main/java/com/alyon/mall/commons/database/context/TenantContextHolder.java
@@ -0,0 +1,11 @@
+package com.alyon.mall.commons.database.context;
+
+/**
+ * @ClassName TenantContextHolder
+ * @Description: TODO
+ * @Author liang
+ * @Date 2020/4/16
+ * @Version V1.0
+ **/
+public class TenantContextHolder {
+}
---
+++ mall-commons/commons-database-spring-boot-starter/src/main/java/com/alyon/mall/commons/database/properties/MybatisPlusAutoFillProperties.java
@@ -0,0 +1,11 @@
+package com.alyon.mall.commons.database.properties;
+
+/**
+ * @ClassName MybatisPlusAutoFillProperties
+ * @Description: TODO
+ * @Author liang
+ * @Date 2020/4/16
+ * @Version V1.0
+ **/
+public class MybatisPlusAutoFillProperties {
+}
