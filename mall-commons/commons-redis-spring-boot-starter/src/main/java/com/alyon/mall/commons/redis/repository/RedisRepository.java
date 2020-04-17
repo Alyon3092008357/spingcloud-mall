2ecf5852693366bbc64579e948775f4a0c3fed92
alyon <1247956257@qq.com>
2020-04-17 23:16:25 +0800

    完善redis插件

---
+++ mall-commons/commons-redis-spring-boot-starter/src/main/java/com/alyon/mall/commons/redis/repository/RedisRepository.java
@@ -0,0 +1,11 @@
+package com.alyon.mall.commons.redis.repository;
+
+/**
+ * @ClassName RedisRepository
+ * @Description: TODO
+ * @Author liang
+ * @Date 2020/4/17
+ * @Version V1.0
+ **/
+public class RedisRepository {
+}
